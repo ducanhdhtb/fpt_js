0ee665936dfb6b3a37548041b0bf9917830cf77d
Your Name <ducanhdhtb@gmail.com>
2018-12-28 09:49:05 +0700

    Update lession 4

--- Lession4/deepcomparision.js
+++ Lession4/deepcomparision.js
@@ -1,114 +1,34 @@
-function deepCompare () {
-    var i, l, leftChain, rightChain;
+Object.equals = function( x, y ) {
+    if ( x === y ) return true;
+      // if both x and y are null or undefined and exactly the same
   
-    function compare2Objects (x, y) {
-      var p;
+    if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+      // if they are not strictly equal, they both need to be Objects
   
-      // remember that NaN === NaN returns false
-      // and isNaN(undefined) returns true
-      if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
-           return true;
-      }
+    if ( x.constructor !== y.constructor ) return false;
+      // they must have the exact same prototype chain, the closest we can do is
+      // test there constructor.
   
-      // Compare primitives and functions.     
-      // Check if both arguments link to the same object.
-      // Especially useful on the step where we compare prototypes
-      if (x === y) {
-          return true;
-      }
+    for ( var p in x ) {
+      if ( ! x.hasOwnProperty( p ) ) continue;
+        // other properties were tested using x.constructor === y.constructor
   
-      // Works in case when functions are created in constructor.
-      // Comparing dates is a common scenario. Another built-ins?
-      // We can even handle functions passed across iframes
-      if ((typeof x === 'function' && typeof y === 'function') ||
-         (x instanceof Date && y instanceof Date) ||
-         (x instanceof RegExp && y instanceof RegExp) ||
-         (x instanceof String && y instanceof String) ||
-         (x instanceof Number && y instanceof Number)) {
-          return x.toString() === y.toString();
-      }
+      if ( ! y.hasOwnProperty( p ) ) return false;
+        // allows to compare x[ p ] and y[ p ] when set to undefined
   
-      // At last checking prototypes as good as we can
-      if (!(x instanceof Object && y instanceof Object)) {
-          return false;
-      }
+      if ( x[ p ] === y[ p ] ) continue;
+        // if they have the same strict value or identity then they are equal
   
-      if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
-          return false;
-      }
+      if ( typeof( x[ p ] ) !== "object" ) return false;
+        // Numbers, Strings, Functions, Booleans must be strictly equal
   
-      if (x.constructor !== y.constructor) {
-          return false;
-      }
-  
-      if (x.prototype !== y.prototype) {
-          return false;
-      }
-  
-      // Check for infinitive linking loops
-      if (leftChain.indexOf(x) > -1 || rightChain.indexOf(y) > -1) {
-           return false;
-      }
-  
-      // Quick checking of one object being a subset of another.
-      // todo: cache the structure of arguments[0] for performance
-      for (p in y) {
-          if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
-              return false;
-          }
-          else if (typeof y[p] !== typeof x[p]) {
-              return false;
-          }
-      }
-  
-      for (p in x) {
-          if (y.hasOwnProperty(p) !== x.hasOwnProperty(p)) {
-              return false;
-          }
-          else if (typeof y[p] !== typeof x[p]) {
-              return false;
-          }
-  
-          switch (typeof (x[p])) {
-              case 'object':
-              case 'function':
-  
-                  leftChain.push(x);
-                  rightChain.push(y);
-  
-                  if (!compare2Objects (x[p], y[p])) {
-                      return false;
-                  }
-  
-                  leftChain.pop();
-                  rightChain.pop();
-                  break;
-  
-              default:
-                  if (x[p] !== y[p]) {
-                      return false;
-                  }
-                  break;
-          }
-      }
-  
-      return true;
+      if ( ! Object.equals( x[ p ],  y[ p ] ) ) return false;
+        // Objects and Arrays must be tested recursively
     }
   
-    if (arguments.length < 1) {
-      return true; //Die silently? Don't know how to handle such case, please help...
-      // throw "Need two or more arguments to compare";
+    for ( p in y ) {
+      if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+        // allows x[ p ] to be set to undefined
     }
-  
-    for (i = 1, l = arguments.length; i < l; i++) {
-  
-        leftChain = []; //Todo: this can be cached
-        rightChain = [];
-  
-        if (!compare2Objects(arguments[0], arguments[i])) {
-            return false;
-        }
-    }
-  
     return true;
   }
