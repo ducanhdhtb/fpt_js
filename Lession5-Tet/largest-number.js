5525196aea3d77ad57a756093bca4592007d4963
Your Name <ducanhdhtb@gmail.com>
2019-01-01 11:36:40 +0700

    Update lession 5 remove

--- Lession5-Tet/largest-number.js
+++ Lession5-Tet/largest-number.js
@@ -1,12 +1,13 @@
 function largestNumber(num) {
+    debugger;
     var NUM = [];
     var res = '';
     
     for (var i = 0; i <num.length; i++) {
-        NUM.push(num[i]+'');
+        NUM.push(num[i]+''); // ["3", "30", "34", "5", "9"]
     }
  
-    NUM.sort(function (a, b) {
+    NUM.sort(function (a, b) { // ["9", "5", "34", "3", "30"]
         var leftRight = a+b;
         var rightLeft = b+a;
         
@@ -20,11 +21,11 @@ function largestNumber(num) {
     });
  
     for (var i = 0; i < NUM.length; i++) {
-        res += NUM[i];
+        res += NUM[i]; // res = "9534330"
     }
  
     return parseInt(res);
  }
  
- console.log(largestNumber([3, 30, 34, 5, 9]));
+ console.log(largestNumber([3, 30, 34, 5, 9])); // 9534330
  console.log(largestNumber([9, 99, 98, 998, 9997]));
