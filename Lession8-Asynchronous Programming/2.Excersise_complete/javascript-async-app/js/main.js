cd6183c75116a93170d3120654bb2541c05191f4
Your Name <ducanhdhtb@gmail.com>
2019-01-16 10:27:31 +0700

    Lession 8 p2

--- Lession8-Asynchronous Programming/2.Excersise_complete/javascript-async-app/js/main.js
+++ Lession8-Asynchronous Programming/2.Excersise_complete/javascript-async-app/js/main.js
@@ -1,29 +1,64 @@
-(function() {
+(function () {
   var app = angular.module('myApp', []);
 
   app.controller('demoCtrl', ['$scope', DemoController]);
 
+  function getData(url) {
+    return new Promise(function (resolve, reject) {
+      $.get(url, function (data) {
+        resolve(data);
+      });
+    });
+  }
+
+  function getPostById(id) {
+    return getData(POST_API + '?userId=' + id);
+  }
+
+  function getCommentByPostId(postId) {
+    return getData(COMMENT_API + '?postId=' + postId);
+  }
+
+  const USER_API = 'https://jsonplaceholder.typicode.com/users';
+  const POST_API = 'https://jsonplaceholder.typicode.com/posts';
+  const COMMENT_API = 'https://jsonplaceholder.typicode.com/comments';
+
   function DemoController($scope) {
-    // 1. Get 10 user
-    $.get('https://jsonplaceholder.typicode.com/users', function(users) {
-  		// if we are here, which means the data is retrieved from the API
-  		console.log(users);
-	});
-	// 2. get posts
-	/*$.get('https://jsonplaceholder.typicode.com/posts?userId=2', function(users) {
-  		// if we are here, which means the data is retrieved from the API
-  		console.log(users);
-	});*/
-  // 3 Get comment from blog
-  /*$.get('https://jsonplaceholder.typicode.com/comments?postId=1', function(users) {
-      // if we are here, which means the data is retrieved from the API
-      console.log(users);
-      for (var i = 0; i < users.length; i++) {
-        console.log(`This is comment number ${i} :`+ users[i]["body"]);
-      }
-  });*/
+    let myUsers;
 
+    // [user1, user2 ,... user10]
+    // [id1, .... id10] => map getPostById
+    // [P(id1), .... P(id10)] => P => P.then(a); [P, ..].map(then)
+    // Promise.all([P(id1), ...P(id10)]).then([id1, ...id10])
+    // [ [post1, post2], ... [post10, post11] ]
+    // [ [P(post1), P(post2)...]... [P(post10), P(post11)] ]
+    // [ P(c1), ... P(c10) ]
+    // [ [], [], [] ]
 
+    getData(USER_API)
+      .then(function (users) {
+        myUsers = users;
+        return Promise.all(users.map(user => user.id).map(getPostById)); // [1, 2, 3, 4, ... 10] => [P(1), ...P(10)]
+      })
+      .then(posts => {
+        return posts.map((post, index) => {
+          myUsers[index].posts = posts;
+          return post.map(post => getCommentByPostId(post.id));
+        });
+      })
+      .then(arrayOfComments => arrayOfComments.map(a => Promise.all(a)))
+      .then(arr => Promise.all(arr))
+      .then(comments => {
+        comments.forEach((comment, index1) => {
+          comment.forEach((c, index2) => {
+            myUsers[index1].posts[index2].comments = c;
+          });
+        });
 
+        $scope.$apply(function () {
+          $scope.users = myUsers;
+        });
+      });
   }
 })();
+
