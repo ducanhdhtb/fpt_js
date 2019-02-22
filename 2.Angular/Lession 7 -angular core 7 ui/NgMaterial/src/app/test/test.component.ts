63ce8e72a8c22e601f50a515259f5e88053a2876
Your Name <ducanhdhtb@gmail.com>
2019-02-22 15:26:36 +0700

    Update finally

---
+++ 2.Angular/Lession 7 -angular core 7 ui/NgMaterial/src/app/test/test.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+
+
+
+export class PizzaPartyAppModule { }
